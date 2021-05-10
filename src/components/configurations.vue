85262b8a61469754381f6a77fce9928a2a8a6bb3
Amer Hero <sooriginal.ba@gmail.com>
2021-05-10 13:51:05 +0200

    basic dropdown hardcoded added

--- src/components/configurations.vue
+++ src/components/configurations.vue
@@ -1,52 +1,117 @@
 <template>
-<div id="myDiagramDiv"></div>
+  <div class="wrapper">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="source-table" style="width:600px">
+      <md-table>
+        <md-table-row>
+          <md-table-head md-numeric>ID</md-table-head>
+          <md-table-head>Column name</md-table-head>
+          <md-table-head>Data type</md-table-head>
+          <md-table-head>Dropdown</md-table-head>
+        </md-table-row>
+
+        <md-table-row v-for="(src, index) in source" :key="src.id" value="src">
+          <md-table-cell md-numeric>{{index + 1}}</md-table-cell>
+          <md-table-cell>{{src.key}}</md-table-cell>
+          <md-table-cell>{{src.type}}</md-table-cell>
+          <md-table-cell>
+            <md-field>
+            <label for="options">options</label>
+            <md-select v-model="src.map" name="option" id="option">
+              <md-option :value="option.key" v-for="option in options" :key="option.id">{{option.key}}</md-option> 
+            </md-select>
+          </md-field>
+          </md-table-cell>
+        </md-table-row>
+
+      </md-table>
+    </div>
+    <div class="source-table" style="width:500px">
+      <md-table>
+        <md-table-row>
+          <md-table-head md-numeric>ID</md-table-head>
+          <md-table-head>Coulmn name</md-table-head>
+          <md-table-head>Data type</md-table-head>
+          <md-table-head></md-table-head>
+        </md-table-row>
+
+        <md-table-row v-for="(option, index) in options" :key="option.id" value="src" style="height:90px" >
+          <md-table-cell md-numeric>{{index + 1}}</md-table-cell>
+          <md-table-cell>{{option.key}}</md-table-cell>
+          <md-table-cell>{{option.type}}</md-table-cell>
+          <md-table-cell class="ic-cell">
+            <a href="#" @click="deleteConfig(option)"><md-icon class="fa fa-close"></md-icon></a>
+            <md-icon class="fa fa-chevron-up"></md-icon>
+            <md-icon class="fa fa-chevron-down"></md-icon>
+          </md-table-cell>
+       
+        </md-table-row>
+
+      </md-table>
+    </div>
+  </div>
 </template>
 
 <script>
-import go from 'gojs'
+  export default {
+    name: 'configurations',
+    data: () => ({
+      source : [
+        {
+          id:1,
+          key:"id",
+          type:"string",
+          map:''
+        },
+        {
+          id:2,
+          key:"iD",
+          type:"string",
+          map:''
+        },
+        {
+          id:3,
+          key:"oPtiOnN",
+          type:"string",
+          map:''
+        },
+        {
+          id:4,
+          key:"OPTION",
+          type:"string",
+          map:''
+        },
+      ],
+      options: [
+        {
+          id:6,
+          key:"id",
+          type:"string"
+        },
+        {
+          id:7,
+          key:"age",
+          type:"number"
+        }
+      ],
 
-export default {
-  name: "App",
-  mounted() {
-    const $ = go.GraphObject.make;  // for conciseness in defining templates
-    const myDiagram =
-      $(go.Diagram, "myDiagramDiv",  // create a Diagram for the DIV HTML element
-        { // enable undo & redo
-          "undoManager.isEnabled": true
-        });
-
-    // define a simple Node template
-    myDiagram.nodeTemplate =
-      $(go.Node, "Auto",  // the Shape will go around the TextBlock
-        $(go.Shape, "RoundedRectangle",
-          { strokeWidth: 0, fill: "white" },  // default fill is white
-          // Shape.fill is bound to Node.data.color
-          new go.Binding("fill", "color")),
-        $(go.TextBlock,
-          { margin: 8 },  // some room around the text
-          // TextBlock.text is bound to Node.data.key
-          new go.Binding("text", "key"))
-      );
-
-    // but use the default Link template, by not setting Diagram.linkTemplate
-
-    // create the model data that will be represented by Nodes and Links
-    myDiagram.model = new go.GraphLinksModel(
-    [
-      { key: "Alpha", color: "lightblue" },
-      { key: "Beta", color: "orange" },
-    ],
-    [
-      { from: "Alpha", to: "Beta" },
-    ]);
+    }),
+    methods: {
+      deleteConfig(config){
+        this.options = this.options.filter(option => option.id != config.id)
+      }
+    }
   }
-
-};
 </script>
 
-<style>
-#myDiagramDiv {
-  width: 100%;
-  height: 500px;
-}
+<style scoped>
+  .wrapper{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .ic-cell{
+    display: flex im !important;
+  }
 </style>
